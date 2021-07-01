0de2d44133ae32d42e5125ef13830d6c8b584cdf
smirnova <dddsssmmm555@mail.ru>
2021-07-01 13:02:14 +0300

    add changes

--- src/test/java/exe2/PasswordEncoderTestTest.java
+++ src/test/java/exe2/PasswordEncoderTestTest.java
@@ -1,8 +1,10 @@
 package exe2;
 
 import junit.framework.TestCase;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class PasswordEncoderTestTest extends TestCase {
@@ -12,5 +14,6 @@ public class PasswordEncoderTestTest extends TestCase {
         PasswordEncoderTest encoder = new PasswordEncoderTest();
 
         Assert.assertEquals("secret: 'mypwd'", encoder.encode("mypwd"));
+        Assert.assertThat(encoder.encode("mypwd"), Matchers.containsString("mypwd"));
     }
 }
