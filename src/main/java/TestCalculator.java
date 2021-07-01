a59c9733b6220cd6840c805728cab094b485cffd
smirnova <dddsssmmm555@mail.ru>
2021-07-01 12:26:58 +0300

    Initial commit

---
+++ src/main/java/TestCalculator.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCalculator {
+
+    @Test
+    public void testCalc() throws Exception {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 5);
+        Assert.assertEquals( 7, result);
+    }
+}
